32578dcc0b441d0abd58cb692426e02881bef2fe
Greg Meess <greg@sutromachine.com>
2019-01-13 23:50:52 -0700

    Add test coverage for snapping high or low

--- test/core/Snapping.Tests.cs
+++ test/core/Snapping.Tests.cs
@@ -35,5 +35,61 @@ namespace g3.Test
             double snapped = Snapping.SnapToNearbyIncrement(4.4, 2, 0.3);
             Assert.AreEqual(4.4, snapped);
         }
+
+        [TestMethod]
+        public void SnapToIncrementLow_LowPos()
+        {
+            double snapped = Snapping.SnapToIncrementLow(4.2, 2);
+            Assert.AreEqual(4, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementLow_LowNeg()
+        {
+            double snapped = Snapping.SnapToIncrementLow(-4.2, 2);
+            Assert.AreEqual(-6, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementLow_HighPos()
+        {
+            double snapped = Snapping.SnapToIncrementLow(5.9, 2);
+            Assert.AreEqual(4, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementLow_HighNeg()
+        {
+            double snapped = Snapping.SnapToIncrementLow(-5.9, 2);
+            Assert.AreEqual(-6, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementHigh_LowPos()
+        {
+            double snapped = Snapping.SnapToIncrementHigh(4.2, 2);
+            Assert.AreEqual(6, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementHigh_LowNeg()
+        {
+            double snapped = Snapping.SnapToIncrementHigh(-4.2, 2);
+            Assert.AreEqual(-4, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementHigh_HighPos()
+        {
+            double snapped = Snapping.SnapToIncrementHigh(5.9, 2);
+            Assert.AreEqual(6, snapped);
+        }
+
+        [TestMethod]
+        public void SnapToIncrementHigh_HighNeg()
+        {
+            double snapped = Snapping.SnapToIncrementHigh(-5.9, 2);
+            Assert.AreEqual(-4, snapped);
+        }
     }
 }
